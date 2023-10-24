5081b9e24ccfc885376fd9f369ae7a4989aaff3c
Pengcheng Zhu <zhupc123@gmail.com>
2023-10-24 12:07:54 +0800

    Problem 0001 -- Two Sum
    Refactor

--- src/main/java/org/example/array/TwoSum.java
+++ src/main/java/org/example/array/TwoSum.java
@@ -5,8 +5,9 @@ import java.util.Map;
 
 public class TwoSum {
 
+    private static Solution solution = new Solution();
+
     public static void main(String[] args) {
-        Solution solution = new Solution();
         int[] nums = new int[3];
         nums[0] = 3;
         nums[1] = 2;
@@ -15,21 +16,21 @@ public class TwoSum {
         System.out.println(result[0] + ", " + result[1]);
     }
 
-}
-
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numsMap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (numsMap.containsKey(target - nums[i])) {
-                int result[] = new int[2];
-                result[0] = i;
-                result[1] = numsMap.get(target - nums[i]);
-                return result;
-            } else {
-                numsMap.put(nums[i], i);
+    private static class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> numsMap = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (numsMap.containsKey(target - nums[i])) {
+                    int result[] = new int[2];
+                    result[0] = i;
+                    result[1] = numsMap.get(target - nums[i]);
+                    return result;
+                } else {
+                    numsMap.put(nums[i], i);
+                }
             }
+            return null;
         }
-        return null;
     }
 }
+
