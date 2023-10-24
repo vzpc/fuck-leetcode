2f7c1d3a72cb60b8f6b35f3a60a3eaee2f17b6b9
Pengcheng Zhu <zhupc123@gmail.com>
2023-10-24 12:07:48 +0800

    Problem 0001 -- Two Sum

---
+++ src/main/java/org/example/array/TwoSum.java
@@ -0,0 +1,35 @@
+package org.example.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[3];
+        nums[0] = 3;
+        nums[1] = 2;
+        nums[2] = 4;
+        int[] result = solution.twoSum(nums, 6);
+        System.out.println(result[0] + ", " + result[1]);
+    }
+
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numsMap.containsKey(target - nums[i])) {
+                int result[] = new int[2];
+                result[0] = i;
+                result[1] = numsMap.get(target - nums[i]);
+                return result;
+            } else {
+                numsMap.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+}
