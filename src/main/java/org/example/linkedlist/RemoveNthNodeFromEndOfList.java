38c9e5ebc6a29e7dfd5b2f42371641a5b28584e2
Pengcheng Zhu <zhupc123@gmail.com>
2023-11-13 10:38:44 +0800

    Problem 0019 -- Remove Nth Node From End of List

---
+++ src/main/java/org/example/linkedlist/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,55 @@
+package org.example.linkedlist;
+
+public class RemoveNthNodeFromEndOfList {
+
+    private static Solution solution = new Solution();
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, null)))));
+        ListNode result = solution.removeNthFromEnd(head, 2);
+        while (true) {
+            System.out.print(result.val);
+            if (result.next != null) {
+                result = result.next;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private static class Solution {
+
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            ListNode dummy = new ListNode(0, head);
+            ListNode pointer1 = head, pointer2 = dummy;
+            for (int i = 0; i < n; i++) {
+                pointer1 = pointer1.next;
+            }
+            while (pointer1 != null) {
+                pointer1 = pointer1.next;
+                pointer2 = pointer2.next;
+            }
+            pointer2.next = pointer2.next.next;
+            return dummy.next;
+        }
+    }
+}
+
+class ListNode {
+
+    int val;
+
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
