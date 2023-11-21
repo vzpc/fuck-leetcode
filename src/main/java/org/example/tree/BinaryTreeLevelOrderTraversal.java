9c95312a0619100b63472bb9697d408fbe612f0b
Pengcheng Zhu <zhupc123@gmail.com>
2023-11-21 18:21:14 +0800

    Problem 0102 -- Binary Tree Level Order Traversal

---
+++ src/main/java/org/example/tree/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,51 @@
+package org.example.tree;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreeLevelOrderTraversal {
+
+    private static Solution solution = new Solution();
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3, null, null), null));
+        List<List<Integer>> result = solution.levelOrder(root);
+        for (List<Integer> level: result) {
+            for (Integer node: level) {
+                System.out.print(node + ",");
+            }
+            System.out.println();
+        }
+    }
+
+    private static class Solution {
+
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> result = new ArrayList<>();
+            if (root == null) {
+                return result;
+            }
+            Deque<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                List<Integer> level = new ArrayList<>();
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    level.add(node.val);
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+                result.add(level);
+
+            }
+            return result;
+        }
+    }
+}
