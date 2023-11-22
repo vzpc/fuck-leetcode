8f8a2a9fb18ab8e6866aa3552595500569787ce3
Pengcheng Zhu <zhupc123@gmail.com>
2023-11-22 18:50:54 +0800

    Problem 0105 -- Construct Binary Tree from Preorder and Inorder Traversal

---
+++ src/main/java/org/example/tree/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,66 @@
+package org.example.tree;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+
+    private static Solution solution = new Solution();
+
+    public static void main(String[] args) {
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+        TreeNode result = solution.buildTree(preorder, inorder);
+        List<Integer> preorderTraversalResult = preorderTraversal(result);
+        for (Integer i : preorderTraversalResult) {
+            System.out.print(i + ",");
+        }
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorderTraversal(root, result);
+        return result;
+    }
+
+    public static void preorderTraversal(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        result.add(root.val);
+        if (root.left != null) {
+            preorderTraversal(root.left, result);
+        }
+        if (root.right != null) {
+            preorderTraversal(root.right, result);
+        }
+    }
+
+    private static class Solution {
+
+        private Map<Integer, Integer> inorderIndexMap = new HashMap<>();
+
+        public TreeNode buildTree(int[] preorder, int[] inorder) {
+            if (preorder == null || preorder.length == 0) {
+                return null;
+            }
+            for (int i = 0; i < inorder.length; i++) {
+                inorderIndexMap.put(inorder[i], i);
+            }
+            return buildTree(preorder, inorder, 0, 0, inorder.length - 1);
+        }
+
+        public TreeNode buildTree(int[] preorder, int[] inorder, int preorderIndex, int inorderStart, int inorderEnd) {
+            if (inorderStart > inorderEnd) {
+                return null;
+            }
+            TreeNode root = new TreeNode(preorder[preorderIndex]);
+            int inorderIndex = inorderIndexMap.get(preorder[preorderIndex]);
+            root.left = buildTree(preorder, inorder, preorderIndex + 1, inorderStart, inorderIndex - 1);
+            root.right = buildTree(preorder, inorder, preorderIndex + inorderIndex - inorderStart + 1, inorderIndex + 1, inorderEnd);
+            return root;
+        }
+    }
+}
