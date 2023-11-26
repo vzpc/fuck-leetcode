2b60df4913d561cbd22fd689ec273de09bf089c4
Pengcheng Zhu <zhupc123@gmail.com>
2023-11-26 21:04:14 +0800

    Problem 0112 -- Path Sum

---
+++ src/main/java/org/example/tree/PathSum.java
@@ -0,0 +1,30 @@
+package org.example.tree;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PathSum {
+
+    private static Solution solution = new Solution();
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3, null, null), null));
+        boolean result = solution.hasPathSum(root, 5);
+        System.out.println(result);
+    }
+
+    private static class Solution {
+
+        public boolean hasPathSum(TreeNode root, int targetSum) {
+            if (root == null) {
+                return false;
+            }
+            if (root.left == null && root.right == null && root.val == targetSum) {
+                return true;
+            }
+            return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+        }
+    }
+}
