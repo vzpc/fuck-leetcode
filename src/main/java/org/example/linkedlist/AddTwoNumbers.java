cc016d4bc8f1f0175b5dc78a423f29a7bd7aec4b
Pengcheng Zhu <zhupc123@gmail.com>
2023-12-17 20:00:06 +0800

    Problem 0002 -- Add Two Numbers

---
+++ src/main/java/org/example/linkedlist/AddTwoNumbers.java
@@ -0,0 +1,47 @@
+package org.example.linkedlist;
+
+public class AddTwoNumbers {
+
+    private static Solution solution = new Solution();
+
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, null)))));
+        ListNode list2 = new ListNode(9, null);
+        ListNode result = solution.addTwoNumbers(list1, list2);
+        while (true) {
+            System.out.print(result.val);
+            if (result.next != null) {
+                result = result.next;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private static class Solution {
+
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode dummy = new ListNode(-1);
+            ListNode pointer = dummy;
+            int carry = 0;
+            while (l1 != null || l2 != null) {
+                int sum = carry;
+                if (l1 != null) {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+                if (l2 != null) {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+                pointer.next = new ListNode(sum % 10);
+                pointer = pointer.next;
+                carry = sum / 10;
+            }
+            if (carry > 0) {
+                pointer.next = new ListNode(carry);
+            }
+            return dummy.next;
+        }
+    }
+}
