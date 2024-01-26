93291b66784e116675d9d04f56f7970344013a73
Pengcheng Zhu <zhupc123@gmail.com>
2024-01-26 21:56:33 +0800

    Problem 0155 -- Min Stack

---
+++ src/main/java/org/example/stack/MinStack.java
@@ -0,0 +1,45 @@
+package org.example.stack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MinStack {
+
+    Deque<Integer> xStack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        xStack = new LinkedList<>();
+        minStack = new LinkedList<>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int val) {
+        xStack.push(val);
+        minStack.push(Math.min(minStack.peek(), val));
+    }
+
+    public void pop() {
+        xStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return xStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.top());
+        System.out.println(minStack.getMin());
+    }
+}
